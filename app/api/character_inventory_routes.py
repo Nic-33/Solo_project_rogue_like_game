da00afd179aaf6848c722e6840393ed7998c26b7
Nicholas Gould <nicgou@gmail.com>
2024-05-25 21:41:30 -0500

    finished inventory routes and form for backend

---
+++ app/api/character_inventory_routes.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, request
+from flask_login import login_required, current_user
+from app.models import db, Inventory as Inv, Character_info
+from app.forms import InvForm
+
+inv_routes = Blueprint('inv', __name__)
+
+@inv_routes.route('/<int:char_id>', methods=['GET'])
+def get_inv_char(char_id):
+    user_id = current_user.to_dict()['id']
+    char_info = Character_info.query.get(char_id)
+    char = char_info.get_gear_inv_id()
+    if user_id == char['user_id']:
+        inv = Inv.query.get(char['inv_id'])
+        return inv.to_dict()
+    else:
+        return {'Error': 'item not found'}, 404
+
+@inv_routes.route('/<int:char_id>', methods=['PUT'])
+def Update_inv(char_id):
+    form = InvForm()
+    user_id = current_user.to_dict()['id']
+    char_info = Character_info.query.filter(Character_info.id==char_id).first()
+    char = char_info.get_gear_inv_id()
+    if user_id == char['user_id']:
+        form['csrf_token'].data = request.cookies['csrf_token']
+        if form.validate_on_submit():
+            inv = Inv.query.get(char['inv_id'])
+            inv.head = form.head.data
+            inv.chest = form.chest.data
+            inv.right = form.right.data
+            inv.left = form.left.data
+            db.session.commit()
+            return inv.to_dict()
+        print(form.errors)
+        return form.errors, 401
+    else:
+        return {'Error': 'item not found'}, 404
+
+@inv_routes.route('/<int:char_id>', methods=['DELETE'])
+def delete_inv_char(char_id):
+    user_id = current_user.to_dict()['id']
+    char_info = Character_info.query.get(char_id)
+    char = char_info.get_gear_inv_id()
+    if user_id == char['user_id']:
+        inv = Inv.query.get(char['inv_id'])
+        db.session.delete(inv)
+        db.session.commit()
+        return {'Delete': "successful"}, 200
+    else:
+        return {'Error': 'item not found'}, 404
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,4 @@ from .signup_form import SignUpForm
 from .run_form import RunForm
 from .gear_form import GearForm
 from .character_info_form import CharForm
+from .inventory_form import InvForm
