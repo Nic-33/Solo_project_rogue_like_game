2bd2db8dc82cb2920225abf5d6d135783b4fd6ef
Nicholas Gould <nicgou@gmail.com>
2024-06-15 14:01:32 -0500

    test 2

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import User
 
 user_routes = Blueprint('users', __name__)
@@ -8,11 +8,9 @@ user_routes = Blueprint('users', __name__)
 @user_routes.route('/')
 @login_required
 def users():
-    """
-    Query for all users and returns them in a list of user dictionaries
-    """
-    users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
+    user_id = current_user.to_dict()['id']
+    user = User.query.get(user_id)
+    return user.to_dict()
 
 
 @user_routes.route('/<int:id>')
