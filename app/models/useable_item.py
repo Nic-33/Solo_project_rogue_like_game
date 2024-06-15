8c0642b3acef315e48018011ef50680965cc7b67
Nicholas Gould <nicgou@gmail.com>
2024-06-15 12:58:18 -0500

    made changes to the models hoping that with fix the issues with render

--- app/models/useable_item.py
+++ app/models/useable_item.py
@@ -7,14 +7,10 @@ class Useable_item (db.Model):
         __table_args__ = {'schema': SCHEMA}
 
 
-    if environment == 'production':
-        id = db.Column(db.Integer(), primary_key=True)
-        user_id = db.Column(db.Integer(), db.ForeignKey(add_prefix_for_prod("users.id")),nullable=False)
-        useable_inv = db.Column(db.String(), nullable=False)
-    else:
-        id = db.Column(db.Integer(), primary_key=True)
-        user_id = db.Column(db.Integer(), db.ForeignKey("users.id"),nullable=False)
-        useable_inv = db.Column(db.String(), nullable=False)
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey(add_prefix_for_prod("users.id")),nullable=False)
+    useable_inv = db.Column(db.String(), nullable=False)
+
 
     users = db.relationship("User", back_populates="useable_items")
 
--- app/models/user_info.py
+++ app/models/user_info.py
@@ -6,18 +6,12 @@ class User_info (db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-    if environment == 'production':
-        id = db.Column(db.Integer(), primary_key=True)
-        user_id = db.Column(db.Integer(),  db.ForeignKey(add_prefix_for_prod("users.id")),nullable=False)
-        battle = db.Column(db.Integer())
-        wins = db.Column(db.Integer())
-        loss = db.Column(db.Integer())
-    else:
-        id = db.Column(db.Integer(), primary_key=True)
-        user_id = db.Column(db.Integer(),  db.ForeignKey("users.id"),nullable=False)
-        battle = db.Column(db.Integer())
-        wins = db.Column(db.Integer())
-        loss = db.Column(db.Integer())
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(),  db.ForeignKey(add_prefix_for_prod("users.id")),nullable=False)
+    battle = db.Column(db.Integer())
+    wins = db.Column(db.Integer())
+    loss = db.Column(db.Integer())
+
 
     users = db.relationship("User", back_populates="user_info")
 
